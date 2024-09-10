4d7d0693d408c3adf72d796c631399fb36152421
mat3usq <mmogielnicki123@gmail.com>
2024-09-11 00:19:22 +0200

    hero title + animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Hero from '@/components/main/Hero'
 import Image from 'next/image'
 
 export default function Home() {
-	return <main className='h-full w-full'>
-    <div className="flex flex-col h-[850px] gap-20">
-      <Hero/>
-    </div>
-  </main>
+	return (
+		<main className="h-full w-full">
+			<div className="flex flex-col h-[850px] gap-20">
+				<Hero />
+			</div>
+		</main>
+	)
 }
